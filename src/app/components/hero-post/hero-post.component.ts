261aa28fd2ab72e163a80cd65a29fabca51aa7ed
Edu-Fortes <eduardofortes.ma@gmail.com>
2024-10-04 11:15:30 -0300

    feat(component): create and initialy style hero-post

---
+++ src/app/components/hero-post/hero-post.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-post',
+  standalone: true,
+  imports: [],
+  templateUrl: './hero-post.component.html',
+  styleUrl: './hero-post.component.css'
+})
+export class HeroPostComponent {
+
+}
