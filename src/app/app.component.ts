9fe7315437e4232427108ac9986dde8365eb50b5
Edu-Fortes <eduardofortes.ma@gmail.com>
2024-10-16 08:03:53 -0300

    feat(component): create and style card-post component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroPostComponent } from './components/hero-post/hero-post.component';
+import { CardPostComponent } from './components/card-post/card-post.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroPostComponent],
+  imports: [RouterOutlet, HeroPostComponent, CardPostComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
