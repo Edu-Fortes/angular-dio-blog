703044a181f37f420da5ccf31c6a175141fce009
Edu-Fortes <eduardofortes.ma@gmail.com>
2024-10-18 07:49:00 -0300

    feat(component): create call-to-action component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroPostComponent } from './components/hero-post/hero-post.component';
 import { CardPostComponent } from './components/card-post/card-post.component';
+import { CallToActionComponent } from './components/call-to-action/call-to-action.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroPostComponent, CardPostComponent],
+  imports: [
+    RouterOutlet,
+    HeroPostComponent,
+    CardPostComponent,
+    CallToActionComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
