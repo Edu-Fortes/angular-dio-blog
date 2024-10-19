e5fac259ff9187d7ee99df3d867eebf514d5c190
Edu-Fortes <eduardofortes.ma@gmail.com>
2024-10-19 07:00:55 -0300

    refact(component): uncople hero-post component to receveive outside data

--- src/app/components/hero-post/hero-post.component.ts
+++ src/app/components/hero-post/hero-post.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-hero-post',
   standalone: true,
   imports: [],
   templateUrl: './hero-post.component.html',
-  styleUrl: './hero-post.component.css'
+  styleUrl: './hero-post.component.css',
 })
 export class HeroPostComponent {
-
+  @Input()
+  photoCover: string = '';
+  @Input()
+  cardTitle: string = '';
+  @Input()
+  cardDescription: string = '';
 }
