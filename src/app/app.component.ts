43f7b72ad6b9d92f77f650f619ddba073374d87f
Edu-Fortes <eduardofortes.ma@gmail.com>
2024-10-19 07:43:27 -0300

    feat(router): define router path to home and content pages component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { HeroPostComponent } from './components/hero-post/hero-post.component';
-import { CardPostComponent } from './components/card-post/card-post.component';
-import { CallToActionComponent } from './components/call-to-action/call-to-action.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    NavbarComponent,
-    HeroPostComponent,
-    CardPostComponent,
-    CallToActionComponent,
-    FooterComponent,
-  ],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
