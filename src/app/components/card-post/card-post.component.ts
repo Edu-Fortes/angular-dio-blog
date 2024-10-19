1d192e6ecb9d63c9279f87e57122f02a064c8f92
Edu-Fortes <eduardofortes.ma@gmail.com>
2024-10-19 07:09:01 -0300

    refact(component): uncople card-post component to receive outside data

--- src/app/components/card-post/card-post.component.ts
+++ src/app/components/card-post/card-post.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-post',
   standalone: true,
   imports: [],
   templateUrl: './card-post.component.html',
-  styleUrl: './card-post.component.css'
+  styleUrl: './card-post.component.css',
 })
 export class CardPostComponent {
-
+  @Input()
+  photoCover: string = '';
+  @Input()
+  cardTitle: string = '';
+  @Input()
+  cardDescription: string = '';
+  @Input()
+  cardDate: string = '';
+  @Input()
+  cardAuthor: string = '';
+  @Input()
+  cardAuthorAvatar: string = '';
 }
