40fbf4c9f9846a672a0fa8e8115714ee51296b72
Edu-Fortes <eduardofortes.ma@gmail.com>
2024-10-26 07:49:38 -0300

    refact(navBar): render menu itens using @for loop

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import menuList from '../../utils/menuList';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-
+  menuList = menuList;
 }
---
+++ src/app/utils/menuList.ts
@@ -0,0 +1,14 @@
+export const menuList = [
+  {
+    id: '1',
+    text: 'Home',
+    href: '',
+  },
+  {
+    id: '2',
+    text: 'Posts',
+    href: 'posts',
+  },
+];
+
+export default menuList;
